a2e22d55dbdeabab8db492b2c0de41be5d8557d6
jaxoncreed <jaxoncreed@gmail.com>
2022-07-07 20:57:59 -0400

    Step 5: Modifying and Saving Data

--- src/ProfilePanel.tsx
+++ src/ProfilePanel.tsx
@@ -1,9 +1,11 @@
-import { FunctionComponent, useEffect, useState } from "react";
+import { FormEvent, FunctionComponent, useCallback, useEffect, useState } from "react";
+import { LinkedDataObject } from "ldo";
 import { FoafProfile } from "./ldo/foafProfile.typings";
 import { FoafProfileFactory } from "./ldo/foafProfile.ldoFactory";
+import { fetch } from "@inrupt/solid-client-authn-browser";
 
 const ProfilePanel: FunctionComponent<{ webId: string }> = ({ webId }) => {
-  const [profile, setProfile] = useState<FoafProfile | undefined>();
+  const [profile, setProfile] = useState<LinkedDataObject<FoafProfile> | undefined>();
   useEffect(() => {
     async function fetchProfile() {
       const rawProfile = await (
@@ -15,9 +17,29 @@ const ProfilePanel: FunctionComponent<{ webId: string }> = ({ webId }) => {
         { baseIRI: webId }
       );
       setProfile(foafProfile);
+      setNameField(foafProfile.name || "");
     }
     fetchProfile();
-  })
+  }, [webId]);
+
+  const [nameField, setNameField] = useState<string>("");
+  const onSubmitNameChange = useCallback(async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (profile) {
+      const modifiedProfile = profile.$clone();
+      modifiedProfile.name = nameField;
+      const response = await fetch(webId, {
+        method: "PATCH",
+        body: await modifiedProfile.$toSparqlUpdate(),
+        headers: {
+          "Content-Type": "application/sparql-update"
+        }
+      });
+      if (response.status === 200) {
+        setProfile(modifiedProfile);
+      }
+    }
+  }, [profile, nameField, webId]);
 
   return (
     <>
@@ -31,6 +53,11 @@ const ProfilePanel: FunctionComponent<{ webId: string }> = ({ webId }) => {
             return <li key={friend["@id"]}>{friend["@id"]}</li>
           })}
         </ul>
+        <form onSubmit={onSubmitNameChange}>
+          <label>Profile Name</label>
+          <input type="text" value={nameField} onChange={(e) => setNameField(e.target.value)} />
+          <input type="submit" value="Change Name" />
+        </form>
       </>) : undefined}
     </>
   );
